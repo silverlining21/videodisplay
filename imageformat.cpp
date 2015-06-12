86154bddc15afa96ee99fb136c5d26a771c1f072
DamonDH <874019350@qq.com>
2015-06-12 17:40:56 +0800

    multi disp video, compile passed

--- imageformat.cpp
+++ imageformat.cpp
@@ -1,1 +1,18 @@
+#include "imageformat.h"
 
+QImage ImageFormat::Mat2QImage(cv::Mat const& srcImage) {
+     QImage dest2 = QImage((uchar*)(srcImage.data), srcImage.cols, srcImage.rows, QImage::Format_RGB888);
+     return dest2;
+}
+
+QImage ImageFormat::MatGray2QImage(cv::Mat const& srcImage) {
+     QImage dest2 = QImage((uchar*)(srcImage.data), srcImage.cols, srcImage.rows, QImage::Format_Indexed8);
+     return dest2;
+}
+
+cv::Mat ImageFormat::QImage2Mat(QImage const& src) {
+     cv::Mat tmp(src.height(),src.width(),CV_8UC3,(uchar*)src.bits(),src.bytesPerLine());
+     cv::Mat result;
+     cvtColor(tmp, result,COLOR_BGR2RGB);
+     return result;
+}
--- imageformat.h
+++ imageformat.h
@@ -1,5 +1,30 @@
 #ifndef IMAGEFORMAT_H
 #define IMAGEFORMAT_H
+#include "mainwindow.h"
+#include <QImage>
 
+/** Image format operations.
+This class contains static functions to convert between OpenCV Mat and Qt QImage formats.
+*/
+class ImageFormat {
+    public:
+    /** Converts color Mat to QImage.
+    @param src OpenCV Mat color image.
+    @return Qt QImage.
+    */
+    static QImage Mat2QImage(cv::Mat const& src);
+
+    /** Converts grayscale Mat to QImage.
+    @param src OpenCV Mat grayscale image.
+    @return Qt QImage.
+    */
+    static QImage MatGray2QImage(cv::Mat const& src);
+
+    /** Converts color QImage to Mat.
+    @param src Qt color QImage.
+    @return Qt OpenCV Mat color image.
+    */
+    static cv::Mat QImage2Mat(QImage const& src);
+};
 #endif // IMAGEFORMAT_H
 
